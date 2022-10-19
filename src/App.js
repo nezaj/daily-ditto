91f2ae0d33f4a968ef7afaddd894d1f25d80d5a1
Joe Averbukh <joeaverbukh@gmail.com>
2022-10-19 17:32:10 -0400

    Persist re-order

--- src/App.js
+++ src/App.js
@@ -20,7 +20,56 @@ function editTodo(x, editList, setEditList) {
   setEditList(editList.filter((id) => id !== x.id));
 }
 
-function onDragEnd(result) {}
+function onDragEnd(result, todos) {
+  const { destination, source, draggableId } = result;
+
+  if (!destination) {
+    return;
+  }
+
+  if (
+    destination.droppableId === source.droppableId &&
+    destination.index === source.index
+  ) {
+    return;
+  }
+
+  let newOrder;
+  if (destination.index === 0) {
+    newOrder = firstOrder(todos);
+  } else if (destination.index === todos.length - 1) {
+    newOrder = lastOrder(todos);
+  } else if (destination.index > source.index) {
+    newOrder =
+      (todos[destination.index].order + todos[destination.index + 1].order) /
+      2.0;
+  } else if (destination.index < source.index) {
+    newOrder =
+      (todos[destination.index - 1].order + todos[destination.index].order) /
+      2.0;
+  } else {
+    console.log("Oi! This shouldn't happen! Ping Joe @ joeaverbukh@gmail.com");
+  }
+
+  const updateId = todos[source.index].id;
+  transact([tx.todos[updateId].update({ order: newOrder })]);
+}
+
+function firstOrder(todos) {
+  if (!todos.length) {
+    return 0;
+  }
+
+  return Math.min(...todos.map((x) => x.order)) - 1;
+}
+
+function lastOrder(todos) {
+  if (!todos.length) {
+    return 0;
+  }
+
+  return Math.max(...todos.map((x) => x.order)) + 1;
+}
 
 // Components
 // -------------
@@ -34,8 +83,7 @@ function Button({ onClick, label }) {
 
 function Main() {
   const data = useQuery({ todos: {} });
-  const todos = data["todos"].sort((a, b) => a.ts - b.ts);
-  console.log(todos.map((x) => x.ts));
+  const todos = data["todos"].sort((a, b) => a.order - b.order);
   const todoRef = useRef(null);
   const [editList, setEditList] = useState([]);
   return (
@@ -43,15 +91,14 @@ function Main() {
       {window.location.hostname === "localhost" && (
         <div className="my-4 text-center bg-teal-200">DEVELOPMENT</div>
       )}
-      <DragDropContext onDragEnd={onDragEnd}>
+      <DragDropContext onDragEnd={(result) => onDragEnd(result, todos)}>
         <Droppable droppableId="todos">
           {(pDrop) => (
             <div ref={pDrop.innerRef} {...pDrop.droppableProps}>
               {todos.map((x, idx) => (
-                <Draggable draggableId={x.id} index={idx}>
+                <Draggable key={x.id} draggableId={x.id} index={idx}>
                   {(pDrag) => (
                     <div
-                      key={x.id}
                       className="my-2"
                       {...pDrag.draggableProps}
                       {...pDrag.dragHandleProps}
@@ -61,7 +108,7 @@ function Main() {
                         <input
                           className="mx-2"
                           type="checkbox"
-                          onClick={(e) => {
+                          onChange={(e) => {
                             transact([
                               tx.todos[x.id].update({
                                 done: x.done === "true" ? "false" : "true",
@@ -123,8 +170,13 @@ function Main() {
                 return;
               }
               const newID = id();
+              const order = lastOrder(todos);
               transact([
-                tx.todos[newID].update({ label, ts: new Date().getTime() }),
+                tx.todos[newID].update({
+                  label,
+                  ts: new Date().getTime(),
+                  order,
+                }),
               ]);
               todoRef.current.value = null;
             }}
@@ -134,7 +186,7 @@ function Main() {
       </span>
       <Button
         onClick={(e) => {
-          const ids = data["todos"].filter((x) => x.title).map((x) => x.id);
+          const ids = todos.map((x) => x.id);
           transact(ids.map((i) => tx.todos[i].delete()));
         }}
         label="Purge"
